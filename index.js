e6bdfbf809e81165e2b2ce35d664c0ed30d7f106
Akych <akychrak@gmail.com>
2020-04-11 14:02:15 +0500

    upload_for_me

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const Dclient = new Discord.Client()
 var cfg = {
-    "tts_channel_id" : "", // text channel id to speech 
+    "tts_channel_id" : "697885963959009370", // text channel id to speech 
     "bot_token" : "", // bot tocken
 }
 const getTTSLink = (text, speeker = "zahar")=>{
@@ -14,6 +14,25 @@ var status = {
     "isPlay" : false,
     "connection" : null
 }
+
+// id : speeker
+var speek_custom = {
+    "259963475000950785" : "nick",
+    "243085229147815936" : "ermil",
+}
+/*
+oksana
+jane	
+omazh	
+zahar	
+ermil	
+silaerkan	
+erkanyavas	
+alyss	
+nick	
+*/
+Date.inSeconds = function() { return Math.floor((new Date().getTime())/1000) }
+
 const play_tts = (channel)=>{
     if(status.isPlay) return;
     let Link = qeue.shift()
@@ -41,15 +60,46 @@ const play_tts = (channel)=>{
         })
     }
 }
+
+
+const command = (msgcontent) =>{
+    let arr = msgcontent.split(" ");
+    let cmd = arr.shift()
+    let text = arr.join(' ');
+    text = text.trim()
+    console.log(cmd,"has",text)
+}
+
 Dclient.on('ready', () => {
+    let old_user_id 
+    let old_user_time 
+
     console.log("Login in")
     Channels["tts"] = Dclient.channels.get(cfg["tts_channel_id"]) 
     Dclient.on('message', (msg) => {
         if(msg.author.bot) return;
         if(msg.channel.id == Channels["tts"].id){
+            command(msg.content)
             let voiceChannel = msg.member.voiceChannel;
             if (!voiceChannel){ msg.channel.send('Вне войс канала доступ к ***tts*** запрещен.').then((m)=>{m.delete(10000);msg.delete(10000)}); return}
-            let Link = getTTSLink(`${msg.author.username} говорит ${msg.content}`)
+            
+            let totts_msg;
+            if(old_user_time > Date.inSeconds()){
+                if(old_user_id != msg.author.id){
+                    old_user_id = msg.author.id
+                    old_user_time = Date.inSeconds() + 10
+                    totts_msg = `${msg.author.username} говорит /${msg.content}`
+                }else{
+                    old_user_time = Date.inSeconds() + 10
+                    totts_msg = msg.content
+                }
+            }else{
+                old_user_id = msg.author.id
+                old_user_time = Date.inSeconds() + 10
+                totts_msg = `${msg.author.username} говорит /${msg.content}`
+            }
+
+            let Link = getTTSLink(totts_msg,speek_custom[msg.author.id])
             qeue.push(Link)
             play_tts(voiceChannel)
         }
