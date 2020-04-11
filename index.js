c2fe5eb2b45c5b21097fd69ca9275f39cd41cfbc
Akych <akychrak@gmail.com>
2020-04-11 14:43:04 +0500

    --

--- index.js
+++ index.js
@@ -20,17 +20,7 @@ var speek_custom = {
     "259963475000950785" : "nick",
     "243085229147815936" : "ermil",
 }
-/*
-oksana
-jane	
-omazh	
-zahar	
-ermil	
-silaerkan	
-erkanyavas	
-alyss	
-nick	
-*/
+
 Date.inSeconds = function() { return Math.floor((new Date().getTime())/1000) }
 
 const play_tts = (channel)=>{
@@ -62,12 +52,53 @@ const play_tts = (channel)=>{
 }
 
 
-const command = (msgcontent) =>{
+
+/*
+oksana
+jane	
+omazh	
+alyss
+silaerkan
+
+
+zahar	
+ermil	
+erkanyavas	
+nick	
+*/
+
+
+const voices = {
+    1 : "oksana",
+    2 : "jane",
+    3 : "omazh",
+    4 : "alyss",
+    5 : "silaerkan",
+
+    6 : "zahar",
+    7 : "ermil",
+    8 : "erkanyavas",
+    9 : "nick"
+}
+
+
+const prefix = "@"
+const commands = {
+    "voice" : (text,channel,author)=>{
+        if(!voices[text]){ channel.send(`Не удалось найти голос с индексом **${text}** используйте от 1 до 9`); return}
+        channel.send("Голос установлен - "+voices[text])
+        speek_custom[author.id] = voices[text]
+    }
+}
+
+const command = (msgcontent,channel,author) =>{
     let arr = msgcontent.split(" ");
     let cmd = arr.shift()
+    cmd = cmd.slice(1)
     let text = arr.join(' ');
     text = text.trim()
-    console.log(cmd,"has",text)
+    if(!commands[cmd]){channel.send("Неизвестная команда - "+cmd); return;}
+    commands[cmd](text,channel,author)
 }
 
 Dclient.on('ready', () => {
@@ -79,10 +110,14 @@ Dclient.on('ready', () => {
     Dclient.on('message', (msg) => {
         if(msg.author.bot) return;
         if(msg.channel.id == Channels["tts"].id){
-            command(msg.content)
             let voiceChannel = msg.member.voiceChannel;
             if (!voiceChannel){ msg.channel.send('Вне войс канала доступ к ***tts*** запрещен.').then((m)=>{m.delete(10000);msg.delete(10000)}); return}
             
+            if(msg.content.startsWith(prefix)){
+                command(msg.content, msg.channel || Channels["tts"],msg.author)
+                return
+            }
+
             let totts_msg;
             if(old_user_time > Date.inSeconds()){
                 if(old_user_id != msg.author.id){
